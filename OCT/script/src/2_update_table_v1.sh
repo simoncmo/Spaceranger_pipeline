b05fcd5925eb819a57e4cd2bafd04c8ee79cd654
cmo <c.mo@wustl.edu>
2022-09-02 13:47:31 -0500

    fix image

--- OCT/script/src/2_update_table_v1.sh
+++ OCT/script/src/2_update_table_v1.sh
@@ -1,5 +1,5 @@
 #cd /diskmnt/primary/Spatial_Transcriptomics/spaceranger_tools/OCT/
 cd '../'
 mkdir table
-wget --no-check-certificate -O table/spaceranger_table_oct.tsv 'https://docs.google.com/spreadsheets/d/1yeY-hHAcOFbB0CT9acUgMRSunaLkV3qr6_1aqcOMBVo/edit#gid=579866082'
+wget --no-check-certificate -O table/spaceranger_table_oct.tsv 'https://docs.google.com/spreadsheets/d/1yeY-hHAcOFbB0CT9acUgMRSunaLkV3qr6_1aqcOMBVo/export?format=tsv&gid=579866082'
 echo "spaceranger_table_oct.tsv updated from Google Sheet"
--- OCT/script/src/4_check_images_v1.sh
+++ OCT/script/src/4_check_images_v1.sh
@@ -1,5 +1,5 @@
 echo "This script check if all image files exist"
-echo "If no MISSING message means everything correct!"
+echo "If no MISSING all showing EXIST then everything correct!"
 #cd /diskmnt/primary/Spatial_Transcriptomics/spaceranger_tools/OCT/
 cd '../'
 SP_TABLE="table/spaceranger_table_oct.tsv"
@@ -8,7 +8,7 @@ IMG_CHECK="check_img.txt"
 awk -F$"\t" 'NR>1 {image_path=$16; progress_status=$21;
 
         if (progress_status=="Running"){
-                printf("[ -f %s ] && echo \"%s exist.\" || echo  \"Image %s MISSING! Please check image name or re-upload.\"\n", image_path, image_path, image_path)
+                printf("[ -f %s ] && echo \"%s EXIST.\" || echo  \"Image %s MISSING! Please check image name or re-upload.\"\n", image_path, image_path, image_path)
         }
 }' $SP_TABLE > $IMG_CHECK
 
